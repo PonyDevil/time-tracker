bc06012110cca47dc889abf27c5454f851ac80de
Krzysztof Rygwelski <mr.rygwelski@gmail.com>
2025-11-15 01:50:47 +0100

    Add Project#total_time and TimeEntry#active?

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,14 @@
 class Project < ApplicationRecord
+    has_many :time_entries, dependent: :destroy
+
+    def total_time_in_seconds
+        time_entries.where.not(end_time:nil).sum { |e| e.end_time - e.start_time}
+    end
+
+    def total_time_formatted
+        seconds = total_time_in_seconds
+        hours = seconds / 3600
+        minutes = (seconds % 3600) / 60
+        "#{hours}:#{minutes.to_s.rjust(2, '0')}h"
+    end
 end
--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,3 +1,11 @@
 class TimeEntry < ApplicationRecord
   belongs_to :project
+
+  def active?
+    end_time.nil?
+  end
+
+  def duration
+    end_time - start_time if end_time
+  end
 end
